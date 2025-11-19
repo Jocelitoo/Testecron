e7aeff5e2ce82c0b18df4b6ec8431b4a59883cbe
Jocelitoo <juninhoclimeres@outlook.com>
2025-11-19 17:15:03 -0300

    novo número adicionado

--- src/app/api/check-vencimentos/route.ts
+++ src/app/api/check-vencimentos/route.ts
@@ -8,5 +8,14 @@ export const GET = async () => {
 
     await sendWhatsAppMessage('Teste cron')
 
+    const sendWhatsAppMessage2 = async (message:string) => {
+        const encoded = encodeURIComponent(message);
+        const url = `https://api.callmebot.com/whatsapp.php?phone=558589836423&text=${encoded}&apikey=6562936`
+
+        await fetch(url).then(() => console.log('Mensagem enviada')).catch(() => console.log('Erro ao enviar mensagem'));
+    }
+
+    await sendWhatsAppMessage2('Teste cron')
+
     return Response.json({ ok: true, value: 123 });
 }
