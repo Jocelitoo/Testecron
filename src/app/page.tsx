b009df172decfae9c9db7d8ec3938a71141f1ccb
Jocelitoo <juninhoclimeres@outlook.com>
2025-11-19 15:51:23 -0300

    teste

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,8 @@ export  default async function Home() {
   // const data = await res.json();
 
   // console.log(data)
+
+  console.log('oi')
   
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
